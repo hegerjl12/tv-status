fef954c81db179ea92336ac7db6ad54eff8ad5f6
hegerjl12 <jared.heger@gmail.com>
2023-01-23 17:32:35 -0800

    added deta base

--- alerts/main.py
+++ alerts/main.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request, abort, render_template, url_for, flash, redirect
+from deta import Deta
+
+
+deta = Deta('b0bjxtd6_PyvkvLdwfvHVm7k7MccimkFX1H3K3ZMr')
+db = deta.Base('SPY')
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
     return "Home Page"
+
+@app.route('/spy_webook', methods=['POST'])
+def spy_webhook():
+    if request.method == 'POST':
+
+        data = request.json
+
+        alert_timeframe = data['timeframe']
+        alert_direction = data['signal']
+        alert_price = data['price']
+
+        db.put({
+            "signal": alert_direction, "price": alert_price
+        })
+
+        return 'success', 200
+    else:
+        abort(400)
+
+
