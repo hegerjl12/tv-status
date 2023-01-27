0839c455d1696f0df45346fb87e67de3c6887496
hegerjl12 <jared.heger@gmail.com>
2023-01-26 18:06:51 -0800

    added two databases for 1m and 15m

--- alerts/main.py
+++ alerts/main.py
@@ -3,17 +3,15 @@ from deta import Deta
 
 app = Flask(__name__)
 
-deta = Deta('b0xrzj95_36AvMwmWFKZuzkk4sZmA3wki6VrAQHNG')
-db = deta.Base('SPY')
+deta = Deta('b0hip04s_76Lucyw7JK6pt3vKtpD4kUmZZfQKfuwo')
+spy15m_db = deta.Base('SPY15m')
+spy1m_db = deta.Base('SPY1m')
 
-test = False
+
 
 @app.route('/', methods=["GET"])
 def index():
-    if test:
-        return "Home Page True"
-    else:
-        return  "Home Page False"
+    return  "Home Page"
 
 @app.route('/spy_webhook', methods=['POST'])
 def spy_webhook():
@@ -24,10 +22,16 @@ def spy_webhook():
         alert_timeframe = data['timeframe']
         alert_direction = data['signal']
         alert_price = data['price']
-        test = True
-        db.put({
-            "signal": alert_direction, "price": alert_price
-        })
+        
+        if alert_timeframe == '1m':
+            spy1m_db.put(
+                {"signal": alert_direction, "price": alert_price},
+                'current' )
+
+        if alert_timeframe == '15m':
+            spy15m_db.put(
+                {"signal": alert_direction, "price": alert_price},
+                'current' )
 
         return 'success', 200
     else:
